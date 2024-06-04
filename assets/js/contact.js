8b9ec2fd8e9ed6aefbcd4eaf969fe0bb44d7082c
Tharchen03 <sherabten098@gmail.com>
2024-06-04 17:57:06 +0600

    update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,7 +4,6 @@ async function sendEmail(event) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    const messageContainer = document.getElementById('messageContainer');
 
     try {
         const response = await fetch('http://localhost:3000/send-email', {
@@ -16,15 +15,31 @@ async function sendEmail(event) {
         });
 
         if (response.ok) {
-            messageContainer.textContent = 'Email sent successfully!';
+            Swal.fire({
+                position: 'bottom-end',
+                icon: 'success',
+                title: 'Email sent successfully!',
+                showConfirmButton: false,
+                timer: 2500
+            });
+            // Reset the form after successful submission
             document.getElementById('contactForm').reset();
-
         } else {
             const errorMessage = await response.text();
-            messageContainer.textContent = `Failed to send email: ${errorMessage}`;
+            Swal.fire({
+                position: 'bottom-end',
+                icon: 'error',
+                title: `Failed to send email: ${errorMessage}`,
+                showConfirmButton: true
+            });
         }
     } catch (error) {
         console.error('Error:', error);
-        messageContainer.textContent = 'Failed to send email. Please try again later.';
+        Swal.fire({
+            position: 'bottom-end',
+            icon: 'error',
+            title: 'Failed to send email. Please try again later.',
+            showConfirmButton: true
+        });
     }
 }
