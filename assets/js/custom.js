87a1e7a1dfbc8635e9f4cf6620a137cf5ad6352a
Tharchen03 <sherabten098@gmail.com>
2024-06-08 11:21:33 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -171,6 +171,35 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	$('.topic-slider').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
+
+
 
 
 
